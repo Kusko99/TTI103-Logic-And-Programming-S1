3a9357972784b25a64bdf2dc171bcc355c188039
Kusko99 <99877193+Kusko99@users.noreply.github.com>
2023-02-25 08:50:39 -0300

    Delete Lab6 directory

--- Lab6/CassaDoSonhos3.0.py
+++
@@ -1,2 +0,0 @@
-#fazer o mesmo da casa dos sonhos 2.0 mas agora eu devo saber quanto eu tenho que guardar para comprar em x meses
-#usano o metodo da bissecção o quanto eu tenho menos o quanto é precisso 
